c7091b9b2cf7f5860005f7fd96b2ffbadbd2c089
kritiray <kkriti44@gmail.com>
2026-04-24 22:05:56 +0530

    added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders something', () => {
-  render(<App />);
-  const element = screen.getByText(/learn react/i);
-  expect(element).toBeInTheDocument();
+describe('App Component', () => {
+
+  test('renders learn react text', () => {
+    render(<App />);
+    const element = screen.getByText(/learn react/i);
+    expect(element).toBeInTheDocument();
+  });
+
 });
